032709e6611dd95ef39c1467ac0d04c0a91fd791
amadhav2021 <amadhav2021@gmail.com>
2020-03-21 12:01:30 -0400

    Navigation app to shift between multiple windows.

---
+++ navigation/main.py
@@ -0,0 +1,28 @@
+# Navigate between multiple screens of an app project
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class MainWindow(Screen):
+    pass
+
+
+class SecondWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("design.kv")
+
+
+class MyMainApp(App):
+    def build(self):
+        return kv
+
+
+if __name__ == '__main__':
+    MyMainApp().run()
