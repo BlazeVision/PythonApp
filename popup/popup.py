3adc01cf736167448b301ec549286d39b09c810a
amadhav2021 <amadhav2021@gmail.com>
2020-03-21 12:17:41 -0400

    Popup app that shows a notification when clicked.

---
+++ popup/popup.py
@@ -0,0 +1,31 @@
+# Popups and notifications
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
+
+
+class Widgets(Widget):
+    @staticmethod
+    def btn():
+        show_popup()
+
+
+class P(FloatLayout):
+    pass
+
+
+class MyApp(App):
+    def build(self):
+        return Widgets()
+
+
+def show_popup():
+    show = P()
+    popup_window = Popup(title='Popup Window', content=show,
+                         size_hint=(None, None), size=(400, 400))
+    popup_window.open()
+
+
+if __name__ == '__main__':
+    MyApp().run()
